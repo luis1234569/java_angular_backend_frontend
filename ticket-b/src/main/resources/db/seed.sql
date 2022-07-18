af8dfd53c52eed52e900e8c71d3fe3d5d3e2470b
luis1234569 <88169915+luis1234569@users.noreply.github.com>
2022-07-18 11:19:04 -0500

    sql

--- ticket-b/src/main/resources/db/seed.sql
+++ ticket-b/src/main/resources/db/seed.sql
@@ -2,3 +2,5 @@
 \c project_db   ingresar a la base
 \d tsticketss  descripcion
 select dni, carrera, modulo, motivo, sugerencia, descripcion,telefono, created from tsticketss;
+select dni, carrera, modulo, motivo, sugerencia, descripcion,telefono, created, updates from tsticketss;
+select tsticket_id, dni, carrera, modulo, motivo, sugerencia, descripcion,telefono, created, updates from tsticketss;
