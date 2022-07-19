57628acc0515a2faf8bd17f918c8b56563c49cb6
luis1234569 <88169915+luis1234569@users.noreply.github.com>
2022-07-19 11:05:56 -0500

    sql and inputs

--- ticket-b/src/main/resources/db/seed.sql
+++ ticket-b/src/main/resources/db/seed.sql
@@ -2,3 +2,28 @@
 \c project_db   ingresar a la base
 \d tsticketss  descripcion
 select tsticket_id, dni, carrera, modulo, motivo, sugerencia, descripcion,telefono, created, updates from tsticketss;
+
+
+
+
+CREATE TABLE tstickets
+(
+    tsticket_id serial NOT NULL , primary key(tsticket_id),
+    dni character varying(15),
+    carrera character varying(100),
+    modulo boolean,
+    motivo character varying(500) ,
+    sugerencia character varying(200) ,
+    descripcion character varying(500) ,
+    screenshot bytea,
+    file bytea,
+    telefono character varying(15),
+    created timestamp with time zone,
+    updates timestamp with time zone,
+    enabled boolean
+)
+
+
+
+
+
